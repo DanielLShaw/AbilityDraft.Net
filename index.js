1b30334b5a4c59bdac60e60dc262d033102c6f76
Daniel Shaw <dls1992@live.co.uk>
2017-07-07 18:49:00 +0100

    Initial Commit
    Created page which hooks into dota 2 api and pulls out all the hero
    images

---
+++ index.js
@@ -0,0 +1,48 @@
+var express = require("express");
+var request = require("request");
+var exphbs  = require('express-handlebars');
+var app = express();
+
+var hbs = exphbs.create();
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+var apiKey = "758D99DAA60FFCA9D8BF39EF6AD51D41";
+
+app.get('/test', function(req, res, next) {
+  var context = {};
+  var url = 'https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v0001/?key=' + apiKey;
+  request(url, function(err, response, body) {
+    if (!err && response.statusCode < 400) {
+      var heroes = [];
+      var parsedResponse = JSON.parse(body);
+      for (var i = 0; i < parsedResponse.result.heroes.length; i++) {
+        var imgNameStr = parsedResponse.result.heroes[i].name.replace('npc_dota_hero_', '');
+        var name = imgNameStr.replace("_"," ").capitalize();
+        var hero = {
+            name:name,
+            image:'http://cdn.dota2.com/apps/dota2/images/heroes/' + imgNameStr + '_sb.png'
+        }
+        heroes.push(hero);
+      }
+      context.heroes = heroes;
+      res.render('test', context);
+    }
+    else {
+      if (response) {
+        console.log(response.statusCode);
+      }
+      next(err);
+    }
+  });
+});
+
+
+app.listen(3000, function () {
+  console.log('AbilityDraft.net is listening on port 3000');
+})
+
+
+String.prototype.capitalize =   function() {
+    return this.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
+};
