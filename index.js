cb324216a0e6c00ce45a65086a644305df152813
Daniel Shaw <dls1992@live.co.uk>
2017-10-21 14:12:30 +0100

    added hero portrait getter
    Now sorts alpha!

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
 var express = require("express");
 var request = require("request");
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
+var path = require("path");
 var app = express();
 
 var hbs = exphbs.create();
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+var publicPath = path.join(__dirname, 'views');
+app.use('/', express.static(publicPath));
+
 var apiKey = "758D99DAA60FFCA9D8BF39EF6AD51D41";
 
-app.get('/test', function(req, res, next) {
+app.get('/test', function (req, res, next) {
   var context = {};
   var url = 'https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v0001/?key=' + apiKey;
-  request(url, function(err, response, body) {
+  request(url, function (err, response, body) {
     if (!err && response.statusCode < 400) {
       var heroes = [];
       var parsedResponse = JSON.parse(body);
       for (var i = 0; i < parsedResponse.result.heroes.length; i++) {
         var imgNameStr = parsedResponse.result.heroes[i].name.replace('npc_dota_hero_', '');
-        var name = imgNameStr.replace("_"," ").capitalize();
+        var name = imgNameStr.replace("_", " ").capitalize();
         var hero = {
-            name:name,
-            image:'http://cdn.dota2.com/apps/dota2/images/heroes/' + imgNameStr + '_sb.png'
+          name: name,
+          image: 'http://cdn.dota2.com/apps/dota2/images/heroes/' + imgNameStr + '_sb.png'
         }
         heroes.push(hero);
       }
-      context.heroes = heroes;
+
+      context.heroes = heroes.sort(alphaSort);
+
       res.render('test', context);
-    }
-    else {
+    } else {
       if (response) {
         console.log(response.statusCode);
       }
@@ -43,6 +48,18 @@ app.listen(3000, function () {
 })
 
 
-String.prototype.capitalize =   function() {
-    return this.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
+String.prototype.capitalize = function () {
+  return this.replace(/(?:^|\s)\S/g, function (a) {
+    return a.toUpperCase();
+  });
 };
+
+function alphaSort(a, b) {
+  if (a.name > b.name) {
+    return 1;
+  }
+  if (a.name < b.name) {
+    return -1;
+  }
+  return 0
+}
